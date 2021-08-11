34be7851ea5c10580867a0e36561e726e7e992ab
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-11 17:52:47 +0530

    changed formatting of test-problem4

--- problems/test/testProblem4.js
+++ problems/test/testProblem4.js
@@ -1,5 +1,4 @@
-const { inventory } = require('../../inventory/inventory');
-const { problem4 } = require('../problem4');
+const { inventory } = require("../../inventory/inventory");
+const { problem4 } = require("../problem4");
 const result = problem4(inventory);
-for( let i=0;i<result.length;i++)
-console.log(result[i]);
+for (let i = 0; i < result.length; i++) console.log(result[i]);
