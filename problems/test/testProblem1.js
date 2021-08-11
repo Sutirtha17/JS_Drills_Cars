1482729fa089d436c243f08f27b8a7945427c838
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-11 11:14:01 +0530

    changed the input value to 33

--- problems/test/testProblem1.js
+++ problems/test/testProblem1.js
@@ -1,4 +1,4 @@
 const { inventory } = require('../../inventory/inventory');
 const { problem1 } = require('../problem1');
-const result = problem1(inventory,22);
+const result = problem1(inventory,33);
 console.log(result);
