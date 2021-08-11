8675235494126ec43545df2a17346cbda3b64a7f
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-11 17:51:47 +0530

    changed formatting of problem4

--- problems/problem4.js
+++ problems/problem4.js
@@ -1,11 +1,9 @@
 module.exports = {
-    problem4: function(inventory)
-    {
-        let years=[];  // initializing an empty array
-        for(let id=0; id< inventory.length; id++)
-            {
-             years.push(inventory[id].car_year); //pushing the years of every car
-            }
-        return years;
+  problem4: function (inventory) {
+    let years = []; // initializing an empty array
+    for (let id = 0; id < inventory.length; id++) {
+      years.push(inventory[id].car_year); //pushing the years of every car
     }
-}
+    return years;
+  }
+};
