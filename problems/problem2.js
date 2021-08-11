33e090b363bdf76c89fadfe5476a19e60c57955b
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-11 15:45:22 +0530

    added an edge case which returns an empty array for problem2

--- problems/problem2.js
+++ problems/problem2.js
@@ -1,6 +1,8 @@
 module.exports = {
     problem2: function (inventory)
     {
+        if(inventory.length==0)
+        return [];
         const lastElement=inventory.length-1;     //    storing the index of last element
         let result="Last Car is a "+ inventory[lastElement].car_make+' '+ inventory[lastElement].car_model;
         return result;  
