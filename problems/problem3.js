885fa1f7624e66256021171846e54e124d3c1868
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-11 17:51:30 +0530

    changed formatting of problem3

--- problems/problem3.js
+++ problems/problem3.js
@@ -1,23 +1,23 @@
 module.exports = {
-    problem3: function(inventory)
+  problem3: function (inventory)
     {
-    if(inventory.length==0)
-    return [];
-    for( let i=0; i<inventory.length;i++)             // using selection-sort
-        {
-             let min=i;                              // storing the index i in min variable initially
-            for( let j=i+1; j<inventory.length;j++)
-             {
-                 if(inventory[min].car_model.toLowerCase()>inventory[j].car_model.toLowerCase()) // calculating the next min element and storing it's index in min
-                    min=j;
-             }
-             if(min!=i)
-             {
-                let temp=inventory[i];
-                inventory[i]=inventory[min]; // if index min is not i, then we swap them
-                inventory[min]=temp;
-             }
+      if (inventory.length == 0) return [];
+      for (let i = 0; i < inventory.length; i++) {
+        let min = i; // storing the index i in min variable initially
+        for (let j = i + 1; j < inventory.length; j++) {
+          if (
+            inventory[min].car_model.toLowerCase() >
+            inventory[j].car_model.toLowerCase()
+          )
+            // calculating the next min element and storing it's index in min
+            min = j;
         }
-        return inventory;
+        if (min != i) {
+          let temp = inventory[i];
+          inventory[i] = inventory[min]; // if index min is not i, then we swap them
+          inventory[min] = temp;
+        }
+      }
+      return inventory;
     }   
 }
