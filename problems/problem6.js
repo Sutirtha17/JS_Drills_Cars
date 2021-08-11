1b5d0d7586bddefad8ddc03abb0963d3df98ae39
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-11 17:52:12 +0530

    changed formatting of problem6

--- problems/problem6.js
+++ problems/problem6.js
@@ -1,14 +1,11 @@
 module.exports = {
-    problem6: function(inventory)
-    {
-    let carsModel=[]; // initializing empty array
-    for(let id=0; id<inventory.length; id++) //6
-    {
-        if( inventory[id].car_make=="Audi" || inventory[id].car_make=="BMW")
-        {
-        carsModel.push(inventory[id]);  // pushing those car's details which have either AUDI or BMW car-make
-        }
+  problem6: function (inventory) {
+    let carsModel = []; // initializing empty array
+    for (let id = 0; id < inventory.length; id++) {
+      if (inventory[id].car_make == "Audi" || inventory[id].car_make == "BMW") {
+        carsModel.push(inventory[id]); // pushing those car's details which have either AUDI or BMW car-make
+      }
     }
-    return carsModel;  // stringify the whole resultant array
-    }
-}
+    return carsModel; // stringify the whole resultant array
+  }
+};
