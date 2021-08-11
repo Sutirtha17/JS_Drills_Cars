4865931db2c956be4c4f4841494bb5b91ffc6622
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-11 17:50:53 +0530

    changed formatting of problem1

--- problems/problem1.js
+++ problems/problem1.js
@@ -1,16 +1,22 @@
-module.exports = { 
-problem1: function (inventory, requiredId)
-{
-    let result=[];     // initializing the result with a message if ID is not present
-    if(inventory.length==0)
-    return [];
-    for(let id=0; id< inventory.length; id++) 
-    {
-        if(inventory[id].id==requiredId)  // checking if any ID is matching with required ID
-        {
-        return "Car " + inventory[id].id + " is a " + inventory[id].car_year + ' ' + inventory[id].car_make + ' ' + inventory[id].car_model;
-        }
+module.exports = {
+  problem1: function (inventory, requiredId) {
+    let result = []; // initializing the result with a message if ID is not present
+    if (inventory.length == 0) return [];
+    for (let id = 0; id < inventory.length; id++) {
+      if (inventory[id].id == requiredId) {
+        // checking if any ID is matching with required ID
+        return (
+          "Car " +
+          inventory[id].id +
+          " is a " +
+          inventory[id].car_year +
+          " " +
+          inventory[id].car_make +
+          " " +
+          inventory[id].car_model
+        );
+      }
     }
     return result;
-}
-}
+  }
+};
