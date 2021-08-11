4b06e3019b10fa55700679ba8867e54b07919c4e
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-11 17:52:01 +0530

    changed formatting of problem5

--- problems/problem5.js
+++ problems/problem5.js
@@ -1,12 +1,9 @@
 module.exports = {
-    problem5: function(inventory,years)
-    {
-        let yearCount=0;
-        for(let id=0; id<years.length; id++) //5
-        {
-            if(inventory[id].car_year<2000)
-            yearCount=yearCount+1;
-        }   
-        return yearCount;
+  problem5: function (inventory, years) {
+    let yearCount = 0;
+    for (let id = 0; id < years.length; id++) {
+      if (inventory[id].car_year < 2000) yearCount = yearCount + 1;
     }
-}
+    return yearCount;
+  }
+};
