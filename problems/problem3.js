714eef4c5209b7740f5e6c33f30bb8c79740db84
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-08-11 10:13:34 +0530

    modified the algorithm of sort function

--- problems/problem3.js
+++ problems/problem3.js
@@ -1,16 +1,21 @@
 module.exports = {
     problem3: function(inventory)
     {
-        for( let i=0; i<inventory.length;i++)             // using Bubble sort
-            for( let j=0; j<inventory.length-i-1;j++)
+    for( let i=0; i<inventory.length;i++)             // using selection-sort
+        {
+             let min=i;                              // storing the index i in min variable initially
+            for( let j=i+1; j<inventory.length;j++)
              {
-                 if(inventory[j].car_model.toLowerCase()>inventory[j+1].car_model.toLowerCase()) // comparing each elements with it's next element
-                 {
-                    let temp=inventory[j+1];
-                    inventory[j+1]=inventory[j]; // if it's greater than any of it's next then we swap them
-                    inventory[j]=temp;
-                 }
+                 if(inventory[min].car_model.toLowerCase()>inventory[j].car_model.toLowerCase()) // calculating the next min element and storing it's index in min
+                    min=j;
              }
+             if(min!=i)
+             {
+                let temp=inventory[i];
+                inventory[i]=inventory[min]; // if index min is not i, then we swap them
+                inventory[min]=temp;
+             }
+        }
         return inventory;
-    }
+    }   
 }
